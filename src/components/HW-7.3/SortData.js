4eb891e69796fdaa7e07e8001c0168bdfb31b259
NazarovAn <naatonchrome@gmail.com>
2020-12-11 15:12:55 +0400

    aggregation task done

--- src/components/HW-7.3/SortData.js
+++ src/components/HW-7.3/SortData.js
@@ -2,21 +2,44 @@ import React from 'react'
 
 export default function SortData(Component) {
   return class extends React.Component {
-    distinguishMonth(date) {
-      return new Date(date).toDateString().slice(4, 7);
-    }
     distinguishYear(date) {
       return new Date(date).getFullYear();
     }
+    distinguishMonth(date) {
+      return new Date(date).toDateString().slice(4, 7);
+    }
 
-    assembleProps() {
-      return ({ list:[
-        ...this.props.list.map(item => ({ ...item, month: this.distinguishMonth(item.date), year: this.distinguishYear(item.date) }))
-      ]});
+    sortBy(array, order) {
+      switch (order) {
+        case 'year':
+          return array.sort((a, b) => b.year - a.year);
+        case 'month':
+          return array.sort((a, b) => new Date(a.date).getMonth() - new Date(b.date).getMonth() );
+        default:
+          return array.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+      }
+    }
+
+    assembleProps(order) {
+      let { list } = this.props;
+      if (order === 'year') {
+        list = list.map(item => ({ ...item, year: this.distinguishYear(item.date) }));
+      } if (order === 'month') {
+        list = list.map(item => ({ ...item, month: this.distinguishMonth(item.date) }));
+      }
+
+      return ({ list: this.sortBy(list, order) });
     }
     
     render() {
-      return <Component { ...this.assembleProps() } />
+      switch (Component.name) {
+        case 'YearTable':
+          return <Component { ...this.assembleProps('year') } />      
+        case 'MonthTable':
+          return <Component { ...this.assembleProps('month') } />          
+        default:
+          return <Component { ...this.assembleProps() } />
+      }
     }
   }
 }
